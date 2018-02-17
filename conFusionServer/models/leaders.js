783fdd4687e2a16903de42cf566c315ef7a959ef
matthew <mb@matthewbarga.com>
2018-02-17 13:53:53 +0900

    finished assignment 2

---
+++ conFusionServer/models/leaders.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+var leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        default: ''
+    },
+    abbr: {
+        type: String,
+        default: ''
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    }
+}, {
+    timestamps: true
+});
+
+var Leaders = mongoose.model('Leader', leaderSchema);
+
+module.exports = Leaders;
