47d3b59808bb25e0050664329cc062366aedb4ce
matthew.barga <mail@matthewbarga.com>
2018-02-26 13:05:00 +0900

    https activation

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -16,6 +16,8 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+const uploadRouter = require('./routes/uploadRouter');
+
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
@@ -53,6 +55,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+
+app.use('/imageUpload',uploadRouter);
+
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
